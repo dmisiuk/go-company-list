81987fbc04daa6e6c96db9e5211878957fd1608f
Dzmitry Misiuk <minsler.by@gmail.com>
2017-01-01 17:37:59 +0300

    added counting clients

--- app.go
+++ app.go
@@ -9,10 +9,13 @@ import (
 )
 
 type Company struct {
-	Id interface{} `bson:"_id"`
-	Name string `bson:"name"`
-	Email string `bson:"email"`
+	Id          interface{} `bson:"_id"`
+	Name        string `bson:"name"`
+	Email       string `bson:"email"`
+	clientCount int
 }
+
+var companyColl, clientColl *mgo.Collection
 
 func main() {
 	dbName := flag.String("db", "test", "mongo database")
@@ -30,23 +33,43 @@ func main() {
 	// Optional. Switch the session to a monotonic behavior.
 	session.SetMode(mgo.Monotonic, true)
 
-	companyCollName:= "company"
-	c := session.DB(*dbName).C(companyCollName)
-	count, err := c.Count()
+	companyCollName := "company"
+	clientCollName := "clients"
+	companyColl = session.DB(*dbName).C(companyCollName)
+	clientColl = session.DB(*dbName).C(clientCollName)
+	count, err := companyColl.Count()
 	if err != nil {
 		log.Fatal(err)
 	}
 	fmt.Println("count of companies:", count)
 
-	allCompanies := make([]Company,0)
+	allCompanies := make([]Company, 0)
 
-	err = c.Find(bson.M{}).All(&allCompanies)
+	err = companyColl.Find(bson.M{}).All(&allCompanies)
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	for _, company := range allCompanies {
+		var idStr string
+		idStr, ok := company.Id.(string)
+		if !ok {
+			oid, ok := company.Id.(bson.ObjectId)
+			if !ok {
+				log.Fatal(err)
+			}
+			idStr = oid.String()
+		}
+		countOfClients, err := countClients(idStr)
+		if err != nil {
+			log.Fatal(err)
+		}
+		company.clientCount = countOfClients
 		fmt.Println(company)
 	}
 
+}
+
+func countClients(companyId string) (int, error) {
+	return clientColl.Find(bson.M{"companyId": companyId}).Count()
 }
