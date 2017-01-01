2fd2832a764fb6f4e95f93c84880ddaf550db20a
Dzmitry Misiuk <minsler.by@gmail.com>
2017-01-01 17:57:54 +0300

    refactored to support running as http server

--- app.go
+++ app.go
@@ -19,6 +19,8 @@ var companyColl, clientColl *mgo.Collection
 
 func main() {
 	dbName := flag.String("db", "test", "mongo database")
+	isHttp := flag.Bool("http", false, "run http server")
+
 	flag.Parse()
 
 	log.Println("using database", *dbName)
@@ -37,37 +39,58 @@ func main() {
 	clientCollName := "clients"
 	companyColl = session.DB(*dbName).C(companyCollName)
 	clientColl = session.DB(*dbName).C(clientCollName)
+
+	if !*isHttp {
+		doAsCLI()
+	}
+
+}
+func doAsCLI() {
+	count := countCompanies()
+	allCompanies := getAllCompanies()
+	fmt.Println("count of companies:", count)
+	for _, company := range allCompanies {
+		fmt.Println(company)
+	}
+}
+func countCompanies() int {
 	count, err := companyColl.Count()
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println("count of companies:", count)
-
+	return count
+}
+func getAllCompanies() []Company {
 	allCompanies := make([]Company, 0)
-
-	err = companyColl.Find(bson.M{}).All(&allCompanies)
+	err := companyColl.Find(bson.M{}).All(&allCompanies)
 	if err != nil {
 		log.Fatal(err)
 	}
-
 	for _, company := range allCompanies {
-		var idStr string
-		idStr, ok := company.Id.(string)
-		if !ok {
-			oid, ok := company.Id.(bson.ObjectId)
-			if !ok {
-				log.Fatal(err)
-			}
-			idStr = oid.String()
-		}
+		idStr := getIdAsString(company)
 		countOfClients, err := countClients(idStr)
 		if err != nil {
 			log.Fatal(err)
 		}
 		company.clientCount = countOfClients
-		fmt.Println(company)
 	}
+	return allCompanies
+}
 
+func getIdAsString(company Company) string {
+	var idStr string
+	idStr, ok := company.Id.(string)
+	if !ok {
+		oid, ok := company.Id.(bson.ObjectId)
+		if !ok {
+			panic("id is not object id")
+		}
+		idStr = oid.String()
+	}
+	if idStr == "" {
+		panic("company does not have id")
+	}
+	return idStr
 }
 
 func countClients(companyId string) (int, error) {
